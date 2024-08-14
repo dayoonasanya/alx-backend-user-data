500a63ab414b6163825464efd32d7a01a914d39d
dayoonasanya <aonasanya@mail.dccc.edu>
2024-08-14 13:55:09 +0000

    create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
+        """
+        # Create a new user instance
+        new_user = User(email=email, hashed_password=hashed_password)
+        
+        # Add the new user to the session
+        self._session.add(new_user)
+        
+        # Commit the session to save the user in the database
+        self._session.commit()
+        
+        # Return the new user
+        return new_user
